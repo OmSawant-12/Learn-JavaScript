7f6602884e0c3679855e3cb704636c454f9db501
OmSawant-12 <sawantom03@gmail.com>
2024-07-14 20:21:27 +0530

    Learning datatypes

---
+++ Basics_01/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all JS code as new version 
+
+// alert('Hello ') This will not work in node JS , used this in browser
+
+let name = 'Om'
+let age = 21
+let isLogged = false 
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true / false
+// null -> standalone value - this is empty value.
+// undefined => when the value is not defined. 
+// symbol => uniqueness 
+
+// object  
+
+// typeof null ==> object 
+// typeof undefined => undefined 
+
+console.log(typeof null);  // show the type of the datatypes
+
